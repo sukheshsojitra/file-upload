ebba373145113dc579b79af843864135d25442fa
Sukhesh Sojitra <ssojitra@catex.com>
2024-12-09 18:22:57 +0530

    remove condition

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { CssBaseline, StyledEngineProvider, ThemeProvider } from '@mui/material'
 import { RootState } from './config/store';
 import { useSelector } from 'react-redux';
 import theme from './themes';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import { useEffect } from 'react';
+//import { useEffect } from 'react';
 import Login from './pages/Login';
 import FileUpload from './pages/FileUpload';
 import DropFile from './pages/DropFile';
 
 function App() {
   const customization = useSelector((state: RootState) => state.theme);
-  const { token, googleToken, microsoftToken } = useSelector((state: RootState) => state.auth);
+  //const { token, googleToken, microsoftToken } = useSelector((state: RootState) => state.auth);
 
   return (
     <StyledEngineProvider injectFirst>
@@ -24,7 +24,7 @@ function App() {
             <Route path='/drop-file' element={<DropFile />} />
             <Route path='/dashboard' element={<Dashboard />} />
           </Routes>
-          <NavigationGuard token={token || googleToken || microsoftToken} />
+          {/* <NavigationGuard token={token || googleToken || microsoftToken} /> */}
         </Router>
       </ThemeProvider>
     </StyledEngineProvider>
@@ -32,18 +32,18 @@ function App() {
 }
 
 // NavigationGuard component to handle token check and navigation
-const NavigationGuard: React.FC<{ token: string | null }> = ({ token }) => {
-  const navigate = useNavigate();
+// const NavigationGuard: React.FC<{ token: string | null }> = ({ token }) => {
+//   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (token) {
-      //  navigate('/dashboard'); // Redirect if token exists
-    } else {
-      navigate('/'); // Redirect to login if no token
-    }
-  }, [token, navigate]);
+//   useEffect(() => {
+//     if (token) {
+//       //  navigate('/dashboard'); // Redirect if token exists
+//     } else {
+//       navigate('/'); // Redirect to login if no token
+//     }
+//   }, [token, navigate]);
 
-  return null; // This component does not render anything
-};
+//   return null; // This component does not render anything
+// };
 
 export default App;
